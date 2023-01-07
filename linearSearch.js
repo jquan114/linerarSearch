aef6ef2fb3310380743f7315e24cd20d7f38f493
JQUAN COLLINS <104362301+jquan114@users.noreply.github.com>
2023-01-07 13:48:18 -0500

    Create linearSearch.js

---
+++ linearSearch.js
@@ -0,0 +1,14 @@
+// this function accepts an array and a value
+//loop through the array and check if the current array element is equal to the value
+//if it is, return the index at which the element is found
+//if the value is never found , return -1
+
+function linearSearch(arr,val){
+    for (let i = 0 ; i < arr.length ; i++ ){
+
+        if(arr[i] === val) return i;   
+    }
+    return -1;
+}
+
+linearSearch([5,4,1,4,3] , 3)
